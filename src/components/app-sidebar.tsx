5d3ebffe1f8f479f33b8f27f6d25caa12a3a8d3f
Fasi Owaiz Ahmed <fasiowaizahmed1234@gmail.com>
2026-01-15 15:30:20 +0530

    Polar payment integrated

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -13,6 +13,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { usePathname, useRouter } from "next/navigation"
 import {authClient} from '@/lib/auth-client'
+import {useHasActiveSubscription} from "@/features/subscriptions/hooks/use-subscription"
 
 import {
   Sidebar,
@@ -52,6 +53,8 @@ const menuItems = [
 export const AppSidebar = () => {
   const pathname = usePathname()
   const router = useRouter()
+  const {hasActiveSubscription, isLoading}=useHasActiveSubscription();
+  console.log("SUB CHECK:", { hasActiveSubscription, isLoading }) //testing
 
   return (
     <Sidebar collapsible="icon">
@@ -104,22 +107,24 @@ export const AppSidebar = () => {
 
       <SidebarFooter>
         <SidebarMenu>
-            <SidebarMenuItem>
-                <SidebarMenuButton
-                    tooltip="Upgrade to Pro"
-                    className="gap-x-4 h-10 px-4"
-                    onClick={() => {}}
-                >
-                    <StarIcon className="h-4 w-4" />
-                    <span>Upgrade to Pro</span>
-                </SidebarMenuButton>
-            </SidebarMenuItem>
+          {!hasActiveSubscription && ! isLoading && (
+              <SidebarMenuItem>
+                  <SidebarMenuButton
+                      tooltip="Upgrade to Pro"
+                      className="gap-x-4 h-10 px-4"
+                      onClick={() => authClient.checkout({slug:"n8n-pro"})}
+                  >
+                      <StarIcon className="h-4 w-4" />
+                      <span>Upgrade to Pro</span>
+                  </SidebarMenuButton>
+              </SidebarMenuItem>
+          )}
 
             <SidebarMenuItem>
                 <SidebarMenuButton
                     tooltip="Billing Portal"
                     className="gap-x-4 h-10 px-4"
-                    onClick={() => {}}
+                    onClick={() => authClient.customer.portal()}
                 >
                     <CreditCardIcon className="h-4 w-4" />
                     <span>Billing Portal</span>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,10 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@/generated/prisma/client";
+import { polar, checkout, portal, usage, webhooks } from "@polar-sh/better-auth";
+import { Polar } from "@polar-sh/sdk";
 import prisma from "@/lib/db";
+import {polarClient} from './polar'
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -11,4 +14,22 @@ export const auth = betterAuth({
         enabled: true,
         autoSignIn:true 
     },
+    plugins: [
+        polar({
+            client: polarClient,
+            createCustomerOnSignUp: true,
+            use: [
+                checkout({
+                    products: [
+                        {
+                            productId: "b2fb20e8-df44-45ef-ac30-6c33d653aab2",
+                            slug: "n8n-pro" // Custom slug for easy reference in Checkout URL, e.g. /checkout/n8n-pro
+                        }
+                    ],
+                    successUrl: process.env.POLAR_SUCCESS_URL,
+                    authenticatedUsersOnly: true
+                })
+            ],
+        })
+    ]
 });
