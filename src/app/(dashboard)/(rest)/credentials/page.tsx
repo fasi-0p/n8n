67a980fd67a285f51c0d81e3fd7ff80d6065fbad
Fasi Owaiz Ahmed <fasiowaizahmed1234@gmail.com>
2026-01-29 18:55:53 +0530

    sever loader page done

--- src/app/(dashboard)/(rest)/credentials/page.tsx
+++ src/app/(dashboard)/(rest)/credentials/page.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import {requireAuth} from '@/lib/auth-utils'
+import {SearchParams} from 'nuqs'
+import {credentialsParamsLoader} from "@/features/credentials/server/params-loader"
+import {prefetchCredentials} from "@/features/credentials/server/prefetch"
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { HydrateClient } from "@/trpc/server";
+type Props={
+  searchParams: Promise<SearchParams>;
+}
 
-
-const page = async () => {
+const page = async ({searchParams}: Props) => {
   await requireAuth();
-  return  <p>Credentials</p>
+
+  const params = await credentialsParamsLoader(searchParams)
+  prefetchCredentials(params)
+
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <p>TODO: Credentials list</p>
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  )
 }
 
 export default page
