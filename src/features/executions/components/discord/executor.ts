d1f8db15146a877b4e8623faf9b449a021f2439e
Fasi Owaiz Ahmed <fasiowaizahmed1234@gmail.com>
2026-01-30 14:35:14 +0530

    updated schema for discord and slack

---
+++ src/features/executions/components/discord/executor.ts
@@ -0,0 +1,126 @@
+import type { NodeExecutor } from "@/features/executions/types";
+import { NonRetriableError } from "inngest";
+import {createGoogleGenerativeAI} from '@ai-sdk/google'
+import {generateText} from "ai"
+import Handlebars from "handlebars";
+import { geminiChannel } from "@/inngest/channels/gemini";
+import prisma from "@/lib/db";
+
+
+Handlebars.registerHelper("json", (context) => {
+  const jsonString = JSON.stringify(context);
+  return new Handlebars.SafeString(jsonString);
+});
+
+type GeminiData = {
+  variableName?: string;
+  credentialId?: string;
+  systemPrompt?: string;
+  userPrompt?: string;
+};
+
+export const GeminiExecutor: NodeExecutor<GeminiData> = async ({
+  data,
+  nodeId,
+  userId,
+  context,
+  step,
+  publish,
+}) => {
+  await publish(
+    geminiChannel().status({
+      nodeId,
+      status: "loading",
+    })
+  );
+
+  if (!data.variableName){
+    await publish(
+      geminiChannel().status({
+        nodeId, status: "error"
+      })
+    )
+    throw new NonRetriableError("Gemini node: Variable name is missing")
+  }
+
+  if (!data.variableName){
+    await publish(
+      geminiChannel().status({
+        nodeId, status: "error"
+      })
+    )
+    throw new NonRetriableError("Gemini node: Credential is missing")
+  }
+
+  if(!data.userPrompt){
+    await publish(
+      geminiChannel().status({
+        nodeId, status: "error"
+      })
+    )
+    throw new NonRetriableError("Gemini node: User prompt is missing")
+  }
+
+
+  const systemPrompt = data.systemPrompt? Handlebars.compile(data.systemPrompt)(context) : "You are a helful assistant";
+
+  const userPrompt = Handlebars.compile(data.userPrompt)(context);
+
+  const credential = await step.run("get-credential", ()=>{
+    return prisma.credential.findUnique({
+      where:{id: data.credentialId, userId},
+      select:{value: true}
+    })
+  })
+
+  if (!credential){
+    await publish(
+      geminiChannel().status({
+        nodeId, status: "error"
+      })
+    )
+    throw new NonRetriableError("Gemini node: credential not found")
+  }
+
+  const google=createGoogleGenerativeAI({
+    apiKey: credential.value,
+  })
+
+  try{
+    const {steps} = await step.ai.wrap(
+      "gemini-generate-text",
+       generateText,
+        {
+          model: google("gemini-2.0-flash"),
+          system: systemPrompt,
+          prompt: userPrompt,
+          experimental_telemetry: {
+            isEnabled: true,
+            recordInputs: true,
+            recordOutputs: true,
+          }
+        })
+
+        const text = steps[0].content[0].type==='text'? steps[0].content[0].text : ""
+
+        await publish(
+          geminiChannel().status({
+            nodeId, status: "success"
+          })
+        )
+
+        return {
+          ...context,
+          [data.variableName]: {
+            aiResponse: text
+          }
+        }
+  }catch (error) {
+    await publish(
+      geminiChannel().status({
+        nodeId, status: "error"
+      })
+    )
+    throw error;
+  }
+};
